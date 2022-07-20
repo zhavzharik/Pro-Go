788f7d2ee12ff8c34139f1347cd697f875c24f83
Andera Bridger <abridger@at-r4.msk.21-school.ru>
2022-07-20 13:44:36 +0300

    defining and using function result, defer

--- functions/main.go
+++ functions/main.go
@@ -48,6 +48,61 @@ func swapValues2(first, second *int) {
 	fmt.Println("After swap:", *first, *second)
 }
 
+func calcTax(price float64) float64 {
+	return price + (price * 0.2)
+}
+
+func swapValues3(first, second int) (int, int) {
+	return second, first
+}
+
+func calcTax1(price float64) float64 {
+	if price > 100 {
+		return price * 0.2
+	}
+	return -1
+}
+
+func calcTax2(price float64) (float64, bool) {
+	if price > 100 {
+		return price * 0.2, true
+	}
+	return 0, false
+}
+
+func calcTotalPrice1(products map[string]float64, minSpend float64) (total, tax float64) {
+	total = minSpend
+	for _, price := range products {
+		if taxAmount, due := calcTax2(price); due {
+			total += taxAmount
+			tax += taxAmount
+		} else {
+			total += price
+		}
+	}
+	return
+}
+
+func calcTotalPrice2(products map[string]float64) (count int, total float64) {
+	count = len(products)
+	for _, price := range products {
+		total += price
+	}
+	return
+}
+
+func calcTotalPrice3(products map[string]float64) (count int, total float64) {
+	fmt.Println("Function started")
+	defer fmt.Println("First defer call")
+	count = len(products)
+	for _, price := range products {
+		total += price
+	}
+	defer fmt.Println("Second defer call")
+	fmt.Println("Function about to return")
+	return
+}
+
 func main() {
 	printPrice1("Kayak", 275, 0.2)
 	printPrice1("KifeJacket", 48.95, 0.2)
@@ -86,4 +141,53 @@ func main() {
 	fmt.Println("Before calling function", val1, val2)
 	swapValues2(&val1, &val2)
 	fmt.Println("After calling function", val1, val2)
+	fmt.Println("")
+
+	products := map[string]float64{
+		"Kayak ":     275,
+		"Lifejacket": 48.95,
+	}
+
+	for product, price := range products {
+		priceWithTax := calcTax(price)
+		fmt.Println("Product:", product, "Price:", priceWithTax)
+	}
+
+	val1, val2 = 10, 20
+	fmt.Println("Before calling function", val1, val2)
+	val1, val2 = swapValues3(val1, val2)
+	fmt.Println("After calling function", val1, val2)
+	fmt.Println("")
+
+	for product, price := range products {
+		tax := calcTax1(price)
+		if tax != -1 {
+			fmt.Println("Product:", product, "Tax:", tax)
+		} else {
+			fmt.Println("Product:", product, "No tax due")
+		}
+	}
+	fmt.Println("")
+
+	for product, price := range products {
+		if taxAmount, taxDue := calcTax2(price); taxDue {
+			fmt.Println("Product:", product, "Tax:", taxAmount)
+		} else {
+			fmt.Println("Product:", product, "No tax due")
+		}
+	}
+	fmt.Println("")
+
+	total1, tax1 := calcTotalPrice1(products, 10)
+	fmt.Println("Total 1:", total1, "Tax 1:", tax1)
+	total2, tax2 := calcTotalPrice1(nil, 10)
+	fmt.Println("Total 2:", total2, "Tax 2:", tax2)
+	fmt.Println("")
+
+	_, total := calcTotalPrice2(products)
+	fmt.Println("Total:", total)
+	fmt.Println("")
+
+	_, total3 := calcTotalPrice3(products)
+	fmt.Println("Total:", total3)
 }
