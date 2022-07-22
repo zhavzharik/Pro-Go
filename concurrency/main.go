fd015922816fabd2af18b712d851eb4b7b916490
Andera Bridger <abridger@at-r4.msk.21-school.ru>
2022-07-22 19:33:33 +0300

    working with channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -4,9 +4,34 @@ import (
 	"fmt"
 )
 
+func receiveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	}
+	fmt.Println("Channel has been closed")
+}
+
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
-	//time.Sleep(time.Second * 5)
+	fmt.Println("CalcStoreTotal function complete")
+	//time.Sleep(time.Second * 3)
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	//var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	//var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+
+	//go DispatchOrders(dispatchChannel)
+	//for {
+	//	if details, open := <-dispatchChannel; open {
+	//		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	//	} else {
+	//		fmt.Println("Channel has been closed")
+	//		break
+	//	}
+	//}
+
 	fmt.Println("main function complete")
 }
--- concurrency/operations.go
+++ concurrency/operations.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-	var channel chan float64 = make(chan float64)
+	var channel chan float64 = make(chan float64, 2)
 	for category, group := range data {
 		go group.TotalPrice(category, channel)
 	}
+	time.Sleep(time.Second * 3)
+	fmt.Println("-- Starting to receive from channel")
 	for i := 0; i < len(data); i++ {
-		storeTotal += <-channel
+		fmt.Println("-- channel read pending", len(channel), "items in buffer, size", cap(channel))
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
@@ -19,10 +26,10 @@ func CalcStoreTotal(data ProductData) {
 func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
 	var total float64
 	for _, p := range group {
-		fmt.Println(category, "product:", p.Name)
 		total += p.Price
-		//time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Println(category, "subtotal:", ToCurrency(total))
+	fmt.Println(category, "channel sending", ToCurrency(total))
 	resultChannel <- total
+	fmt.Println(category, "channel send complete")
 }
