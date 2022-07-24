bb9781eb2d4313e12d7407dd745bbde35dea10e5
Andera Bridger <abridger@at-r4.msk.21-school.ru>
2022-07-24 16:12:10 +0300

    dealing with (un)recoverable errors

--- errorHandling/main.go
+++ errorHandling/main.go
@@ -2,14 +2,76 @@ package main
 
 import "fmt"
 
-func main() {
-	categories := []string{"Watersports", "Chess", "Running"}
-	for _, cat := range categories {
-		total, err := Products.TotalPrice(cat)
-		if err == nil {
-			fmt.Println(cat, "Total:", ToCurrency(total))
+type CategoryCountMessage struct {
+	Category      string
+	Count         int
+	TerminalError interface{}
+}
+
+func processCategories(categories []string, outChan chan<- CategoryCountMessage) {
+	defer func() {
+		if arg := recover(); arg != nil {
+			fmt.Println(arg)
+			outChan <- CategoryCountMessage{
+				TerminalError: arg,
+			}
+			close(outChan)
+		}
+	}()
+	channel := make(chan ChannelMessage, 10)
+	go Products.TotalPriceAsync(categories, channel)
+	for message := range channel {
+		if message.CategoryError == nil {
+			outChan <- CategoryCountMessage{
+				Category: message.Category,
+				Count:    int(message.Total),
+			}
 		} else {
-			fmt.Println(cat, "(no such category)")
+			panic(message.CategoryError)
 		}
 	}
+}
+
+func main() {
+
+	//defer func() {
+	//	if arg := recover(); arg != nil {
+	//		if err, ok := arg.(error); ok {
+	//			fmt.Println("Error:", err.Error())
+	//			panic(err)
+	//		} else if str, ok := arg.(string); ok {
+	//			fmt.Println("Message:", str)
+	//		} else {
+	//			fmt.Println("Panic recovered")
+	//		}
+	//	}
+	//}()
+
+	categories := []string{"Watersports", "Chess", "Running"}
+
+	channel := make(chan CategoryCountMessage)
+	go processCategories(categories, channel)
+
+	for message := range channel {
+		if message.TerminalError == nil {
+			fmt.Println(message.Category, "Total:", message.Count)
+		} else {
+			fmt.Println("A terminal error occured")
+		}
+
+		//if message.CategoryError == nil {
+		//	fmt.Println(message.Category, "Total:", ToCurrency(message.Total))
+		//} else {
+		//	panic(message.CategoryError)
+		//}
+	}
+
+	//for _, cat := range categories {
+	//	total, err := Products.TotalPrice(cat)
+	//	if err == nil {
+	//		fmt.Println(cat, "Total:", ToCurrency(total))
+	//	} else {
+	//		fmt.Println(cat, "(no such category)")
+	//	}
+	//}
 }
--- errorHandling/operations.go
+++ errorHandling/operations.go
@@ -1,14 +1,25 @@
 package main
 
-type CategoryError struct {
-	requestedCategory string
+import (
+	"fmt"
+)
+
+//type CategoryError struct {
+//	requestedCategory string
+//}
+//
+//func (e *CategoryError) Error() string {
+//	return "Category " + e.requestedCategory + " does not exist"
+//}
+
+type ChannelMessage struct {
+	Category      string
+	Total         float64
+	CategoryError error
+	//*CategoryError
 }
 
-func (e *CategoryError) Error() string {
-	return "Category " + e.requestedCategory + " does not exist"
-}
-
-func (slice ProductSlice) TotalPrice(category string) (total float64, err *CategoryError) {
+func (slice ProductSlice) TotalPrice(category string) (total float64, err error) {
 	productCount := 0
 	for _, p := range slice {
 		if p.Category == category {
@@ -17,7 +28,21 @@ func (slice ProductSlice) TotalPrice(category string) (total float64, err *Categ
 		}
 	}
 	if productCount == 0 {
-		err = &CategoryError{requestedCategory: category}
+		err = fmt.Errorf("Cannot find category: %v", category)
+		//err = errors.New("Cannot find category")
+		//err = &CategoryError{requestedCategory: category}
 	}
 	return
+}
+
+func (slice ProductSlice) TotalPriceAsync(categories []string, channel chan<- ChannelMessage) {
+	for _, c := range categories {
+		total, err := slice.TotalPrice(c)
+		channel <- ChannelMessage{
+			Category:      c,
+			Total:         total,
+			CategoryError: err,
+		}
+	}
+	close(channel)
 }
