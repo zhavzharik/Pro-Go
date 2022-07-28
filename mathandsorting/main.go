0cb405b50d9941732a2433886cb2c6ff4dcbdc4e
Svetlana <zhavzharova2000@yandex.ru>
2022-07-28 16:45:02 +0300

    sorting data

--- mathandsorting/main.go
+++ mathandsorting/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sort"
 	"time"
 )
 
@@ -47,6 +48,65 @@ func main() {
 
 	for i, name := range names {
 		Printfln("Index %v: Name: %v", i, name)
+	}
+
+	ints := []int{9, 4, 2, -1, 10}
+	Printfln("Ints: %v", ints)
+	sort.Ints(ints)
+	Printfln("Ints Sorted: %v", ints)
+	floats := []float64{279, 48.95, 19.50}
+	Printfln("Floats: %v", floats)
+	sort.Float64s(floats)
+	Printfln("Floats Sorted: %v", floats)
+	strings := []string{"Kayak", "Lifejacket", "Stadium"}
+	Printfln("Strings: %v", strings)
+	if !sort.StringsAreSorted(strings) {
+		sort.Strings(strings)
+		Printfln("Strings Sorted: %v", strings)
+	} else {
+		Printfln("Strings Already Sorted: %v", strings)
+	}
+
+	ints = []int{9, 4, 2, -1, 10}
+
+	sortedInts := make([]int, len(ints))
+	copy(sortedInts, ints)
+	sort.Ints(sortedInts)
+	Printfln("Ints: %v", ints)
+	Printfln("Ints Sorted: %v", sortedInts)
+
+	indexOf4 := sort.SearchInts(sortedInts, 4)
+	indexOf3 := sort.SearchInts(sortedInts, 3)
+	Printfln("Index of 4: %v", indexOf4)
+	Printfln("Index of 3: %v", indexOf3)
+
+	Printfln("Index of 4: %v (present: %v)", indexOf4, sortedInts[indexOf4] == 4)
+	Printfln("Index of 3: %v (present: %v)", indexOf3, sortedInts[indexOf3] == 3)
+	fmt.Println()
+
+	products := []Product{
+		{"Kayak", 279},
+		{"Lifejacket", 49.95},
+		{"Soccer Ball", 19.50},
+	}
+	ProductSlices(products)
+	for _, p := range products {
+		Printfln("Name: %v, Price: %.2f", p.Name, p.Price)
+	}
+	fmt.Println()
+
+	ProductSlicesByName(products)
+	for _, p := range products {
+		Printfln("Name: %v, Price: %.2f", p.Name, p.Price)
+	}
+	fmt.Println()
+
+	SortWith(products, func(p1, p2 Product) bool {
+		return p1.Name < p2.Name
+	})
+
+	for _, p := range products {
+		Printfln("Name: %v, Price: %.2f", p.Name, p.Price)
 	}
 
 }
