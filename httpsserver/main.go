b5b6fa8ae8daecd4181abbf09a089ba82357175c
Andera Bridger <abridger@at-r2.msk.21-school.ru>
2022-08-15 00:13:29 +0300

    http, https request

--- httpsserver/main.go
+++ httpsserver/main.go
@@ -10,19 +10,37 @@ type StringHandler struct {
 }
 
 func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	Printfln("Method: %v", request.Method)
-	Printfln("URL: %v", request.URL)
-	Printfln("HTTP Version: %v", request.Proto)
-	Printfln("Host: %v", request.Host)
-	for name, val := range request.Header {
-		Printfln("Header: %v, Value: %v", name, val)
-	}
-	Printfln("---")
+	//Printfln("Method: %v", request.Method)
+	//Printfln("URL: %v", request.URL)
+	//Printfln("HTTP Version: %v", request.Proto)
+	//Printfln("Host: %v", request.Host)
+	//for name, val := range request.Header {
+	//	Printfln("Header: %v, Value: %v", name, val)
+	//}
+	//Printfln("---")
+	Printfln("Request for %v", request.URL.Path)
 	io.WriteString(writer, sh.message)
+	//switch request.URL.Path {
+	//case "/fevicon.ico":
+	//	http.NotFound(writer, request)
+	//case "/message":
+	//	io.WriteString(writer, sh.message)
+	//default:
+	//	http.Redirect(writer, request, "/message", http.StatusTemporaryRedirect)
+	//}
 }
 
 func main() {
-	err := http.ListenAndServe(":5000", StringHandler{message: "Hello, World"})
+	http.Handle("/message", StringHandler{"Hello, World"})
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/", http.RedirectHandler("/message", http.StatusTemporaryRedirect))
+	go func() {
+		err := http.ListenAndServeTLS(":5500", "certificate.cer", "certificate.key", nil)
+		if err != nil {
+			Printfln("HTTPS Error: %v", err.Error())
+		}
+	}()
+	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		Printfln("Error: %v", err.Error())
 	}
